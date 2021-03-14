ae7ef97f6a9fb209007feba7bc77a1248273c8d5
Developkim <developkim@me.com>
2021-03-14 17:38:30 +0900

    youtube full screen embeded

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,49 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import '../lib/cities'
+import getCitiesData from '../lib/cities'
 
-export default function Home() {
+export async function getStaticProps(context) {
+  const cities = getCitiesData()
+  return {
+    props: {
+      cities: cities,
+    }
+  }
+}
+
+function getIndexCity(cities) {
+  return cities[Math.floor(Math.random() * cities.length)] 
+}
+
+function getStrEmbededSrc(city){
+  return `https://www.youtube.com/embed/${city.id}?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=1`;  
+}
+
+
+export default function Home({ cities }) {
+  const indexCityRandom = getIndexCity(cities) 
+  const embedSrc = getStrEmbededSrc(indexCityRandom)
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Walking Tour</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="video-background">
+        <div className="video-foreground">
+          <iframe src={embedSrc} frameBorder="0" allowFullScreen></iframe>
         </div>
+      </div>
+      <main className={styles.main}>
+        <h1>{indexCityRandom.name}</h1>
+        <ul>
+          {cities.map(({name, url}) => (
+            
+            <li>{name} - {url}</li>
+            
+          ))}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
